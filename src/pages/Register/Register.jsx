83ea1dc8ae90fea66aa965bd964a9fe2b591135d
tanjil9840 <142922591+tanjil9840@users.noreply.github.com>
2025-02-07 23:10:16 +0600

    first commit

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <div className="text-center lg:text-left">
+      
+      <p className="py-6">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+    </div>
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <div className="card-body">
+      <h1 className="text-5xl font-bold">Register!</h1>
+        <fieldset className="fieldset">
+          <label className="fieldset-label">Email</label>
+          <input type="email" className="input" placeholder="Email" />
+          <label className="fieldset-label">Password</label>
+          <input type="password" className="input" placeholder="Password" />
+          <div><a className="link link-hover">Forgot password?</a></div>
+          <button className="btn btn-neutral mt-4">Register</button>
+        </fieldset>
+      </div>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default Register;
---
+++ src/router/router.jsx
@@ -0,0 +1,26 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home/Home";
+import Register from "../pages/Register/Register";
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout> </MainLayout> ,
+      children:[
+        {
+            path:'/',
+            element:<Home> </Home>
+        },
+        {
+            path:'/register',
+            element:<Register> </Register>
+        }
+      ]
+    },
+  ]);
+
+  export default router
